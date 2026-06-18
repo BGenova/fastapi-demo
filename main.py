6d0c83047a64fbe0c7160d43d9429470e6ce768d
BrunoGENOVA <brunogenova.dev@gmail.com>
2026-06-18 11:24:50 +0200

    Add user routes: list users and fetch user by ID

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Hello FastAPI"}
+
+@app.get("/users")
+def get_users():
+    return [
+        {"id": 1, "name": "John"},
+        {"id": 2, "name": "Jane"}
+    ]
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return {"id": user_id}
